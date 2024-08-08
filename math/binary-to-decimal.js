9338c9dbd41e5867f46d3d255db57fa34f642c6e
Monicah Koina <monicahkoina1@gmail.com>
2024-08-08 17:34:19 +0300

    binary to decimal

---
+++ math/binary-to-decimal.js
@@ -0,0 +1,10 @@
+//Convert the binary number 110011 to a decimal
+
+//define function
+function bin_to_dec(bstr){
+    return parseInt((bstr + '').replace(/[^01]/gi, ''), 2);
+    /*parseIntconverts the binary string to a decimal integer
+    the regular expression (/[^01]/gi) removes any characters that are not 0/1 from the binary string
+    */
+}
+console.log(bin_to_dec(110011));//output is 51
